9181a5430a4b1c82ebfe07d982c5e97381449cd1
Storm Llamas <storm_sia_llamas@yahoo.com>
2023-11-25 22:50:33 +0800

    modify initial app ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,14 @@ import { useEffect } from "react";
 
 import axios from "axios";
 
-import "./App.css";
-import logo from "./logo.svg";
-
 import "./assets/scss/main.scss";
 
 function App() {
   useEffect(() => {
-    axios.get("http://localhost:8000/bookings/1", {});
+    axios.get("http://localhost:8000/bookings/1");
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div id="app"></div>;
 }
 
 export default App;
